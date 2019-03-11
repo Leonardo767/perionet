13e903d10c46404f89ee086b64ef5610a388243d
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-03-11 03:45:27 -0500

    Added Flash Messages

--- sample/server.py
+++ sample/server.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, url_for, flash, redirect
 app = Flask(__name__)
 
 # consider uncommenting the following if this goes online:
-# app.config['SECRET_KEY'] = '38d8626329795e446589fb7e670f6797'
+app.config['SECRET_KEY'] = '38d8626329795e446589fb7e670f6797'
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    flash('hello', 'success')
+    flash('test', 'danger')
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
