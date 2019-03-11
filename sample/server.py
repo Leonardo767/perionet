e9d89b9a8c815a1b27cb9a67687e2bc7373ee929
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2019-03-11 02:34:02 -0500

    Initial Structure

---
+++ sample/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, url_for, flash, redirect
+app = Flask(__name__)
+
+# consider uncommenting the following if this goes online:
+# app.config['SECRET_KEY'] = '38d8626329795e446589fb7e670f6797'
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
